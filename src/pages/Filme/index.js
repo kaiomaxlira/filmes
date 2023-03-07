271dd3dac08ba7cc5477c9a2ed533dc751908a26
kaiomaxlira <kaiomax01@gmail.com>
2023-03-06 21:19:25 -0300

    Projeto filmaria finalizado

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,60 +1,84 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import './filme-info.css';
 import api from '../../services/api';
+import { toast } from 'react-toastify'
 
-function Filme(){
+function Filme() {
     const { id } = useParams();
+    const navigate = useNavigate();
+
     const [filme, setFilme] = useState({});
     const [loading, setLoading] = useState(true);
 
-    useEffect(()=>{
-        async function loadFilme(){
-            await api.get(`/movie/${id}`,{
-                params:{
+    useEffect(() => {
+        async function loadFilme() {
+            await api.get(`/movie/${id}`, {
+                params: {
                     api_key: "57e8f4826160c54d7bed990827a2b38e",
                     language: "pt-BR",
                 }
             })
-            .then((response)=>{
-                setFilme(response.data);
-                setLoading(false);
-            })
-            .catch(()=>{
-                console.log("FILME NÃO ENCONTRADO")
-            })
+                .then((response) => {
+                    setFilme(response.data);
+                    setLoading(false);
+                })
+                .catch(() => {
+                    console.log("FILME NÃO ENCONTRADO");
+                    navigate("/", { replace: true });
+                    return;
+                })
         }
 
         loadFilme();
 
 
-        return() => {
+        return () => {
             console.log("componentes desmontados")
         }
-    }, [])
+    }, [navigate, id])
 
 
-    if(loading){
-        return(
+    function salvarFilme() {
+        const minhaLista = localStorage.getItem("@primeflix")
+
+        let filmesSalvos = JSON.parse(minhaLista) || [];
+
+        const hasFilme = filmesSalvos.some((filmesSalvo) => filmesSalvo.id === filme.id)
+
+        if (hasFilme) {
+           toast.warn("Este filme já esta na sua lista")
+            return;
+        }
+
+        filmesSalvos.push(filme);
+        localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos));
+        toast.success("Filme salvo com sucesso!")
+
+    }
+
+
+    if (loading) {
+        return (
             <div className="filme-info">
                 <h1>Carregando detalhes...</h1>
             </div>
         )
     }
 
-    return(
+    return (
         <div className="filme-info">
             <h1>{filme.title}</h1>
-            <img  src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
 
             <h3>Sinopse</h3>
             <span>{filme.overview}</span>
             <strong>Avaliação {filme.vote_average} / 10</strong>
 
             <div className="area-buttons">
-                <button>Salva</button>
+                <button onClick={salvarFilme}>Salva</button>
                 <button>
-                    <a href="#">
+                    <a target="blank" rel="external" href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>
                         Trailer
                     </a>
                 </button>
